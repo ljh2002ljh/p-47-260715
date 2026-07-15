a67dcfacfbf0521807c06394059230539d94b423
ljh2002ljh <aeb1119@naver.com>
2026-07-15 12:40:53 +0900

    secornd commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public  static  void main(String[] args){
         System.out.println("== 명언 앱 ");
         System.out.println("명령) 종료");
+        System.out.println(123);
     }
 }
